3b07dea9cbfb5ef34ef7434fd39810ff0d1ddc80
Mingu Kim <mkim245@gmail.com>
2022-07-07 20:05:54 -0400

    completed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,22 @@
 const request = require('request');
-const breedName = process.argv[2];
+// const breedName = process.argv[2];
+
+const fetchBreedDescription = function(breedName, callback) {
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
   if (error) {
-    console.log(error);
-    return;
+    // console.log(error);
+    return callback(error, null);
+    // return;
   }
   const data = JSON.parse(body);
   if (data.length > 0) {
-    console.log(data[0].description);
+    // console.log(data[0].description);
+    return callback(null, data[0].description);
   } else {
-    console.log("breed is not found");
+    // console.log("breed is not found");
+    return callback("breed is not found", null);
   }
 });
+};
+
+module.exports = { fetchBreedDescription };
